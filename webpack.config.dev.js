3cb7e62da12eb2175f5d5b4a543978741353fc7d
Nuke Goldstein <nukegoldstein@gmail.com>
2017-03-06 12:29:25 +0200

    Update webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -11,24 +11,5 @@ module.exports = webpackMerge(commonConfig, {
         publicPath: 'js/app/',
         filename: 'bundle.js',
         chunkFilename: '[id].chunk.js'
-    },
-
-    module: {
-        loaders: [{
-            test: /\.ts$/,
-            loaders: [
-                'awesome-typescript-loader',
-                'angular2-template-loader',
-                'angular2-router-loader'
-            ]
-        }]
-    },
-
-    plugins: [
-        new webpack.ContextReplacementPlugin(
-            // The (\\|\/) piece accounts for path separators in *nix and Windows
-            /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
-            './src' // location of your src
-        )
-    ]
+    }
 });
